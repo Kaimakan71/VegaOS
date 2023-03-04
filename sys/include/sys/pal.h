6ec41254804368e1d3084882ff6ef0a31370086d
IanMarcoMoffett <svr.mkall@gmail.com>
2023-03-03 20:21:57 -0500

    pal: Use PAL instead of ASM instructions directly

---
+++ sys/include/sys/pal.h
@@ -0,0 +1,43 @@
+/*
+ *  author: Ian Marco Moffett.
+ *  description: Processor abstraction layer.
+ *
+ *  Abstracts instructions or processor specific
+ *  things into single macros or functions.
+ */
+
+#ifndef _SYS_PAL_H_
+#define _SYS_PAL_H_
+
+#include <sys/cdefs.h>
+
+#if defined(__x86_64__)
+
+/*
+ *  halt() only halts the current processor
+ *  and it will continue if an interrupt
+ *  fires. To fully halt the CPU ensure
+ *  you use global_irq_disable() first.
+ */
+
+#define halt()                  __asm("hlt")
+
+/*
+ *  Prevents the processor from responding
+ *  to external interrupts.
+ *
+ *  NOTE: This can cause a significant performance
+ *        impact, use judiciously.
+ */
+
+#define global_irq_disable()    __asm("cli")
+
+/*
+ *  Allows the processor to respond
+ *  to interrupts.
+ */
+
+#define global_irq_enable()     __asm("sti")
+
+#endif    /* __x86_64__  */
+#endif    /* _SYS_PAL_H_ */
--- sys/kern/init.c
+++ sys/kern/init.c
@@ -1,11 +1,12 @@
 /* author: Ian Marco Moffett */
 
 #include <sys/cdefs.h>
+#include <sys/pal.h>
 
 __dead void _start(void)
 {
   for (;;)
   {
-    __asm("hlt");
+    halt();
   }
 }
