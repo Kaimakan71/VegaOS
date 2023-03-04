7a24f9e8f935b306ab506dcfbbe3a643a604627f
IanMarcoMoffett <svr.mkall@gmail.com>
2023-03-04 11:13:03 -0500

    kernel: Init exceptions and load IDT

--- sys/kernel/core/init.c
+++ sys/kernel/core/init.c
@@ -4,6 +4,8 @@
 #include <sys/pal.h>
 #include <sys/printk.h>
 #include <sys/module.h>
+#include <amd64/exceptions.h>
+#include <amd64/idt.h>
 
 #define VEGA_COPYRIGHT "Copyright (c) 2023 Ian Marco Moffett"
 
@@ -12,6 +14,9 @@ MODULE("kernel_init");
 __dead void _start(void)
 {
   printk("VegaOS v%s - %s\n\n", VEGA_VERSION, VEGA_COPYRIGHT);
+
+  idt_load();
+  exceptions_init();
 
   for (;;)
   {
