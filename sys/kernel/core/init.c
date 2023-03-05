4b1b1e5e988604c40337d77021dc857febf73343
IanMarcoMoffett <svr.mkall@gmail.com>
2023-03-04 22:34:47 -0500

    kernel: Update init.c

--- sys/kernel/core/init.c
+++ sys/kernel/core/init.c
@@ -4,24 +4,43 @@
 #include <sys/pal.h>
 #include <sys/printk.h>
 #include <sys/module.h>
+#include <sys/types.h>
+#include <mm/pmm.h>
 
 #if defined(__x86_64__)
-#include <amd64/exceptions.h>
-#include <amd64/idt.h>
+# include <amd64/exceptions.h>
+# include <amd64/idt.h>
+#elif defined(__aarch64__)
+  /* TODO */
 #endif
 
 #define VEGA_COPYRIGHT "Copyright (c) 2023 Ian Marco Moffett"
 
 MODULE("kernel_init");
 
+#if defined(__aarch64__)
+static void aarch64_init(void)
+{
+}
+#elif defined(__x86_64__)
+static void amd64_init(void)
+{
+  idt_load();
+  exceptions_init();
+}
+#endif
+
 __dead void _start(void)
 {
   printk("VegaOS v%s - %s\n\n", VEGA_VERSION, VEGA_COPYRIGHT);
 
 #if defined(__x86_64__)
-  idt_load();
-  exceptions_init();
+  amd64_init();
+#elif defined(__aarch64__)
+  aarch64_init();
 #endif
+
+  pmm_init();
 
   for (;;)
   {
