a92ef54016a8f160714380ddfbd0e9bb89cb7f04
IanMarcoMoffett <svr.mkall@gmail.com>
2023-03-05 22:48:17 -0500

    kernel/aarch64: Fix broken VMM implementation

--- sys/kernel/mm/vmm_aarch64.c
+++ sys/kernel/mm/vmm_aarch64.c
@@ -8,10 +8,12 @@
 #include <sys/module.h>
 #include <sys/printk.h>
 #include <sys/cdefs.h>
+#include <string.h>
 
 #if defined(__aarch64__)
 MODULE("vmm");
 
+#define PT_4K_GRANULE    0x3
 #define PT_FLAG_VALID    ((size_t)1 << 0)
 #define PT_FLAG_TABLE    ((size_t)1 << 1)
 #define PT_FLAG_4K_PAGE  ((size_t)1 << 1)
@@ -23,16 +25,14 @@ MODULE("vmm");
 #define PT_FLAG_ACCESS   ((size_t)1 << 10)
 #define PT_FLAG_WB       ((size_t)0 << 2)
 
-
-static uintptr_t get_next_level(uintptr_t pagemap_phys, size_t idx,
-                                uint8_t alloc)
+static uintptr_t next_level(uintptr_t pagemap_phys, size_t idx, uint8_t alloc)
 {
   uintptr_t *pagemap = (uintptr_t *)(pagemap_phys + VMM_HIGHER_HALF);
+  size_t entry = pagemap[idx];
 
-  if (pagemap[idx] & PT_FLAG_VALID)
+  if ((entry & PT_FLAG_VALID) != 0)
   {
-    uintptr_t phys = pagemap[idx] & PT_ADDR_MASK; 
-    return phys;
+    return entry;
   }
 
   if (!alloc)
@@ -41,35 +41,16 @@ static uintptr_t get_next_level(uintptr_t pagemap_phys, size_t idx,
   }
 
   uintptr_t next_level = pmm_alloc(1);
-  pagemap[idx] = next_level  | PT_FLAG_VALID
-                             | PT_FLAG_USER
-                             | PT_FLAG_TABLE;
+  memset((void*)next_level, 0, 4096);
+  pagemap[idx] = next_level
+                 | PT_FLAG_TABLE
+                 | PT_FLAG_VALID
+                 | PT_FLAG_USER
+                 | PT_FLAG_INNER_SH
+                 | PT_FLAG_ACCESS
+                 | PT_FLAG_WB;
+
   return next_level;
-}
-
-static uintptr_t get_pml1(uintptr_t pagemap_phys, uintptr_t virt,
-                          uint8_t alloc)
-{
-
-  size_t pml4_index = (virt >> 39) & 0x1FF;
-  size_t pml3_index = (virt >> 30) & 0x1FF;
-  size_t pml2_index = (virt >> 21) & 0x1FF;
-
-  uintptr_t pml3 = get_next_level(pagemap_phys, pml4_index, alloc);
-
-  if (pml3 == 0)
-  {
-    return 0;
-  }
-
-  uintptr_t pml2 = get_next_level(pml3, pml3_index, alloc);
-
-  if (pml2 == 0)
-  {
-    return 0;
-  }
-  
-  return get_next_level(pml2, pml2_index, alloc);
 }
 
 void vmm_map_page(uintptr_t pagemap_phys, uintptr_t virt, uintptr_t phys,
@@ -83,23 +64,30 @@ void vmm_map_page(uintptr_t pagemap_phys, uintptr_t virt, uintptr_t phys,
    */
   size_t actual_flags = PT_FLAG_VALID
                         | PT_FLAG_4K_PAGE
-                        | PT_FLAG_USER
                         | PT_FLAG_INNER_SH
-                        | PT_FLAG_WB
                         | PT_FLAG_ACCESS;
 
-  if (!(flags & VMM_WRITABLE))
+  if ((flags & VMM_WRITABLE) == 0)
   {
     actual_flags |= PT_FLAG_READONLY;
   }
 
-  if (flags & VMM_NX)
+  if ((flags & VMM_USER) != 0)
   {
-    actual_flags |= PT_FLAG_XN;
+    actual_flags |= PT_FLAG_USER;
   }
-  
-  uintptr_t pml1 = get_pml1(pagemap_phys, virt, 1);
-  ((uintptr_t*)pml1)[GET_PML1_IDX(virt)] = phys | actual_flags;
+
+  size_t pml0_index = (virt >> 39) & 0x1FF;
+  size_t pml1_index = (virt >> 30) & 0x1FF;
+  size_t pml2_index = (virt >> 21) & 0x1FF;
+  size_t pml3_index = (virt >> 12) & 0x1FF;
+
+  uintptr_t pml1 = next_level(pagemap_phys, pml0_index, 1);
+  uintptr_t pml2 = next_level(pml1, pml1_index, 1);
+  uintptr_t pml3 = next_level(pml2, pml2_index, 1);
+
+  uintptr_t *pml3_virt = (uintptr_t *)(pml3 + VMM_HIGHER_HALF);
+  pml3_virt[pml3_index] = actual_flags | phys; 
 }
 
 #endif    /* __aarch64__ */
